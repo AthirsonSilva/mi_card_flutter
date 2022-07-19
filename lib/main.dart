135b5013f3e94f727df53cddb09413e43f862f58
Athirson Silva <athirsonarceus@gmail.com>
2022-07-19 14:35:27 -0300

    containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Container(
+              height: 100.0,
+              width: 100.0,
+              margin: EdgeInsets.fromLTRB(100.0, 150.0, 0.0, 20.0),
+              padding: EdgeInsets.all(32.0),
+              color: Colors.white,
+              child: Text('Hello'),
+            ),
+          )),
+    );
+  }
 }
