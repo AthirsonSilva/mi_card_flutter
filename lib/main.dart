ffae40db238a0158e9ce6de2288ddab9474c387e
Athirson Silva <athirsonarceus@gmail.com>
2022-07-26 11:52:25 -0300

    Contact info

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,50 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 1.2,
                 ),
               ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(9.0),
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      '+55 11 9 5760-7177',
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(9.0),
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      'athirsonarceus@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
